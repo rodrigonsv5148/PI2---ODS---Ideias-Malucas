64466c900432da9f24130a36c19a18f5d8f38001
Rodrigo <rodrigonsv5148@gmail.com>
2024-04-19 09:35:20 -0300

    Att
    Nenhum sistema importante completo a implementação

--- Assets/Assets/Scripts/Específicos/Porta.cs
+++ Assets/Assets/Scripts/Específicos/Porta.cs
@@ -7,17 +7,36 @@ using UnityEngine.Audio;
 
 public class Porta : MonoBehaviour
 {
-    [SerializeField] private bool spawnandoPersonagem = false;
+    [SerializeField] private bool interativo = false;
+    
+    //Variáveis relativas à parte de audio 
     [SerializeField] private List<ScriptableSons> scriptableAudio;
+    [SerializeField] private AudioSource audioSource;
     private ScriptableSons scriptableAudioProximo;
     private ScriptableSons scriptableAudioPassos;
     private ScriptableSons scriptableAudioPorta;
-
-    [SerializeField]private AudioSource audioSource;
     private int qteAudiosProximo;
     private int qteAudiosPassos;
     private int qteAudiosPorta;
     private float tempoAudio;
+    //-----------------------------------------------
+
+    // Relacionados aos personagens
+    [SerializeField] private ScriptablePersonagens listaPersonagens;
+    [SerializeField] private ScriptablePersonagens personagemLocationBase;
+    private int qtePersonagens;
+    private int charactereAtualNumber;
+    List<int> personagensUsados;
+    private GameObject[] personagensInGame = new GameObject[4];
+    private GameObject[] papelInGame = new GameObject[4];
+
+    //----------------------------------------------
+
+    // Relacionados ao papel
+    [SerializeField] private GameObject papel;
+    [SerializeField] private GameObject papelLocationBase;
+    
+    //---------------------------------------------------
 
     private void Start()
     {
@@ -30,24 +49,37 @@ public class Porta : MonoBehaviour
         qteAudiosPassos = qteAudios(scriptableAudioPassos);
         qteAudiosPorta = qteAudios(scriptableAudioPorta);
 
+        qtePersonagens = listaPersonagens.Characteres.Count;  
     }
 
     //Abrir a porta
     public void OnMouseDown()
     {
-        if (spawnandoPersonagem == false)
+        if (interativo == false)
         {
+            
             //spawnandoPersonagem = true; // Variável de controle para não ativar o mesmo evento simultâneamente
             Debug.Log("ativou efeito");
 
             tempoAudio = playAudios(qteAudiosProximo, scriptableAudioProximo);
             StartCoroutine(tempoDeEspera(tempoAudio));
 
+            tempoAudio = playAudios(qteAudiosPorta, scriptableAudioPorta);
+            //Aqui que entra a animação (preciso da animação p fazer isso, acho q placeholder n ficaria legal)
+            StartCoroutine(tempoDeEspera(tempoAudio));
+
             tempoAudio = playAudios(qteAudiosPassos, scriptableAudioPassos);
             StartCoroutine(tempoDeEspera(tempoAudio));
 
-            tempoAudio = playAudios(qteAudiosPorta, scriptableAudioPorta);
-            StartCoroutine(tempoDeEspera(tempoAudio));
+            charactereAtualNumber = Random.Range(0, qtePersonagens);
+            while (personagensUsados.Contains(charactereAtualNumber)) 
+            {
+                charactereAtualNumber = Random.Range(0, qtePersonagens);
+            }
+            personagensUsados.Add(charactereAtualNumber);
+
+            // tenho a lista atualizada e o valor do personagem que vou pegar na lista de assets
+
 
             Debug.Log("Aquiiiiiii " + tempoAudio);
         }
@@ -72,8 +104,8 @@ public class Porta : MonoBehaviour
         int numeroAudio = Random.Range(0, qteAudios);
         float tempoAudio;
         
-        audioSource.clip = scriptable.audios[numeroAudio];
-        tempoAudio = scriptable.audios[numeroAudio].length; // 
+        audioSource.clip = scriptable.audios[numeroAudio]; // seta o clip no audio source
+        tempoAudio = scriptable.audios[numeroAudio].length; // pega o tempo do audio
 
         audioSource.Play();
         return(tempoAudio);
