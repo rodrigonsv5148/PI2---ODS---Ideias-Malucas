73bf78e83e05ea62bb8cb4db9ee8e6954dc35c30
Rodrigo <rodrigonsv5148@gmail.com>
2024-06-12 10:37:48 -0300

    att
    quase pronto

---
+++ Assets/Assets/Scripts/Gerais/ControleMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ControleMenu : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    } 
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
